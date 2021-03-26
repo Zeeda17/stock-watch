43e1607b840c63a05b4e482ae51a3d5119422739
Evan duBois <duboisevan@gmail.com>
2021-03-26 16:24:35 -0400

    updated api inputs and refactored code

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -4,39 +4,49 @@ require 'rubygems'
 module StocksHelper
   include HTTParty
 
-  def getStockAutoComplete(search_input, region_input)
+  def getStockAutoComplete(symbol_input, region_input, range_input)
     key = Rails.application.credentials.rapidAPI[:key]
     host = Rails.application.credentials.rapidAPI[:host]
-    valid_regions = ['US','BR','AU','CA','FR','DE','HK','IN','IT','ES','GB','SG']
-    default_region = 'US'
+
+    regions = {
+      :valid => ['US','BR','AU','CA','FR','DE','HK','IN','IT','ES','GB','SG'],
+      :default => 'US'
+    }
+    intervals = {
+      :valid => ['1m','2m','5m','15m','60m','1d'], # Valid for API but this app not build to support these currently
+      :default => '1d'
+    }
+    ranges = {
+      :valid => ['1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'],
+      :default => '5d'
+    }
+
     self.class.base_uri "https://" + host
 
-    if (validateStockInput(search_input))
-      search_input.gsub!(/[^a-zA-Z]/, '')
+    if (validateStockInput(symbol_input))
+      symbol_input.gsub!(/[^a-zA-Z]/, '')
     end
     
-    if (!validateStockInput(search_input))
+    if (!validateStockInput(symbol_input))
       # no need to continue
       return
     end
 
-    if (validateStockInput(region_input))
-      region_input.gsub!(/[^a-zA-Z]/, '')
-    end
-
-    if (!validateStockInput(region_input) || (!valid_regions.include? region_input.upcase))
-      region_input = default_region
-    end
+    region_input = formatInputForAPI(region_input, regions)
+    range_input = formatInputForAPI(range_input, ranges)
+    # interval = formatInputForAPI(interval, intervals) # add this if the code gets updating to handle this choice
     
     headers = {
       # replace nil with key here to allow the API to work
       # this is a stopgap to limit my api hits
-      "x-rapidapi-key" => nil, #key, 
+      "x-rapidapi-key" => key, 
       "x-rapidapi-host" => host
     }
-    body = { 
-      "q" => search_input,
-      "region" => region_input
+    body = {
+      "interval" => interval,
+      "symbol" => symbol_input,
+      "region" => region_input,
+      "range" => range_input
     }
 
     #takes base_uri and concats first param to it for API endpoint
@@ -44,6 +54,16 @@ module StocksHelper
       '/auto-complete',
       :query => body,
       :headers => headers)
+  end
+
+  def formatInputForAPI(input, allowed_values)
+    if (validateStockInput(input))
+      input.gsub!(/[^a-zA-Z]/, '')
+    end
+
+    if (!validateStockInput(input) || (!allowed_values[:valid].include? input.upcase))
+      input = allowed_values[:default]
+    end
   end
 
   # is data good?
